8d37d295f0b35377b3fb4ef4ba0c10b01d153f76
Naja <113134845+najamoe@users.noreply.github.com>
2024-03-04 14:57:11 +0000

    started working on functions

--- script.js
+++ script.js
@@ -2,62 +2,15 @@
 window.addEventListener("load", start);
 
 function start (){
-    makeBoardClickable();
+   
 }
 
-//-----------VIEW-----------
-function selectCell(row, col){
-    writeToCell(row, col, 1);
-    displayBoard();
+function make2dArray(cols, rows){
+
 }
 
-function makeBoardClickable(){
-    document.querySelector('#board')
-    .addEventListener("click", boardClicked)
+let grid;
+
+function setup(){
+    grid = make2dArray(10, 10);
 }
-
-
-
-function boardClicked(event) {
-   const cell= event.target;
-   const row = cell.dataset.row;
-   const col = cell.dataset.col;
-
-   console.log(`clicked on row: ${row} col: ${col}`);
-   selectCell(row, col);
-}
-
-function displayBoard(){
-    for (let row = 0; row < 3; row++){
-        for (let col = 0; col < 3; col ++){
-            const value = readFromCell(row, col);
-            const cell = document
-            .querySelector(`[data-row="${row}"][data-col="${col}"]`);
-
-
-            switch(value) {
-                case 0: cell.textContent = " "; break;
-                case 1: cell.textContent = "X"; break;
-                case 2: cell.textContent = "O"; break;
-            }
-        }
-    }
-}
-
-//------------MODEL--------------
-
-const model = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-];
-
-function writeToCell(row, col, value){
-    model[row][col] = value;
-}
-
-function readFromCell(row, col) {
-    return model[row][col];
-}
-
-
